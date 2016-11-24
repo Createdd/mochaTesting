29eb1c6e1e616a5ca2e6441323099b3bc6817358
Daniel Deutsch <deudan1010@gmail.com>
2016-11-24 18:49:16 +0100

    implementing a web server

--- test/server.js
+++ test/server.js
@@ -1,6 +1,9 @@
 var expect  = require("chai").expect;
 var request = require("request");
 
+//Nothing will fail, but, at the same time, no expectation will be checked. This happens because we didn't give Mocha enough time to wait for the requests to finish. In other words, the code inside the request's callback is never actually executed.
+
+
 describe("Color Code Converter API", function() {
 
   describe("RGB to Hex conversion", function() {
